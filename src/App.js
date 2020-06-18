e82de4e3a9360867742331c316f1d79b8b4f80ef
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-06-18 11:23:03 +0100

    changed uri

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Footer from './components/Footer';
 // Apollo Client Setup
 const client = new ApolloClient({
   // uri: "http://localhost:3001/graphql" //apollo knows we will be making requests to this end-point from our application
-  uri: "https://techworld-api.herokuapp.com/" 
+  uri: "https://techworld-api.herokuapp.com/graphql" 
 })
 
 const App = () => {
